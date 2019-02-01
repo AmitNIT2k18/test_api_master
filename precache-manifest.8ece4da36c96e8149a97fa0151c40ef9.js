ed2beddb32ba6446894757b6ec1d6a40b765be8b
Pratik Kalathiya <pratik.kalathiya@gmail.com>
2019-02-01 11:26:45 +0530

    Updates

---
+++ precache-manifest.8ece4da36c96e8149a97fa0151c40ef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29f4b48143e6afbb665c",
+    "url": "/test_api_master/static/css/main.3706e501.chunk.css"
+  },
+  {
+    "revision": "29f4b48143e6afbb665c",
+    "url": "/test_api_master/static/js/main.29f4b481.chunk.js"
+  },
+  {
+    "revision": "ea947d66b816256e03f5",
+    "url": "/test_api_master/static/js/1.ea947d66.chunk.js"
+  },
+  {
+    "revision": "7eff920d44f916d59b43",
+    "url": "/test_api_master/static/js/runtime~main.7eff920d.js"
+  },
+  {
+    "revision": "7eb134f9af9f3ec70f88a408ac211ab8",
+    "url": "/test_api_master/index.html"
+  }
+];
